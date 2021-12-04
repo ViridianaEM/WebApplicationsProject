c19886a50c0fb418b8917997e7eef6abe564beca
ViridianaEM <viridiana_em.uwu@hotmail.com>
2021-12-04 10:27:05 -0600

    se agrega el proyecto

---
+++ Final2/Models/Entities/StudentsContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Final2.Models.Entities
+{
+    public partial class StudentsContext : DbContext
+    {
+        public StudentsContext()
+        {
+        }
+
+        public StudentsContext(DbContextOptions<StudentsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Data> Datas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(connectionString: "MSSqlServer");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Modern_Spanish_CI_AS");
+
+            modelBuilder.Entity<Data>(entity =>
+            {
+                entity.ToTable("DataS");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Birthday).HasColumnType("date");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Fname)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Lname)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Major).HasMaxLength(50);
+
+                entity.Property(e => e.Password).HasMaxLength(50);
+
+                entity.Property(e => e.Quarter).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
